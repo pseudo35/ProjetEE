21c5efc04d80f02efae7c7c0138333ad1686c8b1
Lab <Lab@w00tw00t.home>
2023-03-23 22:42:12 +0100

    modification faite sur la servlet MaServlet

--- src/main/java/fr/eni/javaee/projetee/MaServlet.java
+++ src/main/java/fr/eni/javaee/projetee/MaServlet.java
@@ -27,7 +27,7 @@ public class MaServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Bonjour tout le monde");
+		response.getWriter().append("Bonjour tout le monde !");
 	}
 
 	/**
